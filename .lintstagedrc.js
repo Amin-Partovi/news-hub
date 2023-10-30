30b1524d385018f0b8a4f52dc916326058fc7c3d
Amin Partovi <amin.frontend@gmail.com>
2023-10-30 21:29:33 +0330

    build: add linter and code quality configurations

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `yarn eslint --fix -- ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
