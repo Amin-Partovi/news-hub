a471f8042f7020f7b39e8a136ac20664b2c550d6
Amin Partovi <amin.frontend@gmail.com>
2023-11-01 00:52:50 +0330

    feat: add query client configuration

--- src/queries/ReactQueryProvider.tsx
+++ src/queries/ReactQueryProvider.tsx
@@ -1,7 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-const queryClient = new QueryClient();
+const DEFAULT_CACHE_TIME = 5 * 60 * 1000;
+const DEFAULT_STALE_TIME = 1 * 60 * 1000;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: DEFAULT_CACHE_TIME,
+      staleTime: DEFAULT_STALE_TIME,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      retry: false,
+    },
+  },
+});
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
